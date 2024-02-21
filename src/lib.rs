7e385db27db503259bd10cbe56be6ded07a2f00a
Vhyrro <vhyrro@gmail.com>
2024-02-21 12:49:29 +0100

    feat: add documentation, only spawn the norgopolis instance when the target address is local

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,38 @@
+//! This crate provides functionality to easily connect to a `norgopolis` instance and interact with
+//! its modules.
+//!
+//! This Rust crate provides a simple and lightweight layer for communicating with norgopolis.
+//! To establish a connection, use the `connect` function. By default Norgopolis runs on port `62020`:
+//!
+//! ```rs
+//! use norgopolis_client;
+//!
+//! #[tokio::main]
+//! async fn main() {
+//!     let connection = norgopolis_client::connect(&"localhost".into(), &"62020".into())
+//!         .await
+//!         .expect("Unable to connect to server!");
+//!
+//!     // Invokes a specific module's function without any parameters.
+//!     // The closure will be executed for every return value provided. Return values are streamed back
+//!     // over time, hence the `await`.
+//!     connection.invoke("module-name", "function-name", None, |response: YourExpectedResponse| println!("{:#?}", response))
+//!         .await
+//!         .unwrap();
+//! }
+//! ```
+//!
+//! If the `autostart-server` feature flag is enabled, this client will look for a binary called `norgopolis-server`
+//! on the host system and will auto-execute it if a connection could not be initially established.
+//!
+//! The server will be forked into a separate system process and will automatically shut down after 5 minutes
+//! of inactivity.
+
 use futures::FutureExt;
 use std::{
     future::Future,
     io::{BufReader, Read},
+    net::ToSocketAddrs,
     process::{Command, Stdio},
 };
 
@@ -11,9 +42,15 @@ use norgopolis_protos::client_communication::{forwarder_client::ForwarderClient,
 use serde::de::DeserializeOwned;
 use tonic::{transport::Channel, Request, Response, Status, Streaming};
 
+/// Defines a connection to a Norgopolis instance.
 pub struct ConnectionHandle(ForwarderClient<Channel>);
 
 impl ConnectionHandle {
+    /// Invokes a function of a given module running under Norgopolis.
+    /// Returns a future to the response stream.
+    ///
+    /// It's recommended to use the non-raw functions if you do not need greater control
+    /// over the data being sent.
     pub fn invoke_raw(
         &mut self,
         module: String,
@@ -27,6 +64,13 @@ impl ConnectionHandle {
         }))
     }
 
+    /// Invokes a function of a given module running under Norgopolis.
+    ///
+    /// On every received message a callback will be executed with the raw
+    /// MessagePack return data.
+    ///
+    /// It's recommended to use the non-raw functions if you do not need greater control
+    /// over the data being sent.
     pub async fn invoke_raw_callback<F>(
         &mut self,
         module: String,
@@ -52,6 +96,19 @@ impl ConnectionHandle {
         Ok(())
     }
 
+    /// High-level function to invoke a given module's function.
+    ///
+    /// Will execute a callback on every received return message, but
+    /// will also automatically deserialize the received MessagePack into
+    /// a struct of your choice.
+    ///
+    /// Example:
+    /// ```rs
+    ///  // Automatically deserialize the MessagePack into a String.
+    ///  connection.invoke("module-name".to_string(), "function-name".to_string(), None, |response: String| println!("{}", response))
+    ///      .await
+    ///      .unwrap();
+    ///  ```
     pub async fn invoke<TargetStruct, F>(
         &mut self,
         module: String,
@@ -78,6 +135,10 @@ impl ConnectionHandle {
         Ok(())
     }
 
+    /// Invokes a function of a given module running under norgopolis.
+    ///
+    /// Instead of streaming return values back over time, this function waits until all possible
+    /// return values have been received and then returns a vector of outputs.
     pub async fn invoke_collect<TargetStruct>(
         &mut self,
         module: String,
@@ -100,17 +161,27 @@ impl ConnectionHandle {
     }
 }
 
+/// Establish a connection with a running Norgopolis instance.
+///
+/// If the `autostart-server` feature flag is enabled, will attempt to also spawn a Norgopolis
+/// instance if one is not already running.
 pub async fn connect(ip: &String, port: &String) -> anyhow::Result<ConnectionHandle> {
+    let address = format!("{}:{}", ip, port);
+
     Ok(ConnectionHandle(
-        match ForwarderClient::connect("http://".to_string() + ip + ":" + port).await {
+        match ForwarderClient::connect("http://".to_string() + &address).await {
             Ok(connection) => connection,
             Err(err) => {
-                if cfg!(feature = "autostart-server") {
-                    let mut command = Command::new("norgopolis-server")
+                if cfg!(feature = "autostart-server")
+                    && address
+                        .to_socket_addrs()?
+                        .all(|socket| socket.ip().is_loopback())
+                {
+                    let command = Command::new("norgopolis-server")
                         .stdout(Stdio::piped())
                         .spawn()?;
 
-                    if let Some(stdout) = command.stdout.take() {
+                    if let Some(stdout) = command.stdout {
                         let mut buffer = BufReader::new(stdout);
                         let mut str = [b' '; 5];
 
@@ -141,10 +212,10 @@ mod tests {
             .await
             .unwrap()
             .invoke(
-                "test-module".to_string(),
-                "func-name".to_string(),
-                None,
-                |response: (String,)| println!("{}", response.0),
+                "hello-world".to_string(),
+                "echo".to_string(),
+                Some(MessagePack::encode("hello".to_string()).unwrap()),
+                |response: String| println!("{}", response),
             )
             .await
             .unwrap();
